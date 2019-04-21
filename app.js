0567609ec69340386c198c9c3849c95f9b313308
josiahmoon <32020472+josiahmoon@users.noreply.github.com>
2019-04-21 19:04:41 -0400

    updated story route

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
-app.use('/create', require('./routes/create'));
+app.use('/story', require('./routes/story'));
 
 const PORT = process.env.PORT || 3499;
 
